b243d4cb878ffe974a7ffd38e4eafa7b9d9b38b7
Gabriel <gabrielabdul1@gmail.com>
2020-07-08 14:44:29 +0000

    corrected code to meet requirements

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # test strings: hbn, hbon, hbtn, hbttn, hbtttn, hbttttn
 # match result: hbn, hbtn, hbttn, hbtttn, hbttttn
-puts ARGV[0].scan([hbt]+n/).join
+puts ARGV[0].scan(hbt*n/).join
