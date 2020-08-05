abcd830ceb0d9921d6481f9feb6543139dc93c68
Gabriel <gabrielabdul1@gmail.com>
2020-08-05 01:07:13 +0000

    added comment

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# set up your client SSH configuration file so that you can connect to a server without typing a password
+
 file_line {'ssh_config':
     path  => '~/etc/ssh/ssh_config',
     match => '*Pass*Auth*yes'
