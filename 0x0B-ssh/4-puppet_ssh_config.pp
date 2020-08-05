88371495696e631cfb445e185b835b817d01619e
Gabriel <gabrielabdul1@gmail.com>
2020-08-05 01:09:51 +0000

    added commas

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 
 file_line {'ssh_config':
     path  => '~/etc/ssh/ssh_config',
-    match => '*Pass*Auth*yes'
+    match => '*Pass*Auth*yes',
     line  => '#   PasswordAuthentication no'
 }
 
 file_line {'ssh_config':
     path  => '~/etc/ssh/ssh_config',
-    match => '*Id*F*_rsa'
+    match => '*Id*F*_rsa',
     line  => '#   IdentityFile ~/.ssh/holberton'
 }
