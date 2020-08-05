87523f4cc3df25d99fbf9cd0f751ff096779745b
Gabriel <gabrielabdul1@gmail.com>
2020-08-05 01:28:09 +0000

    major fixes

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 # set up your client SSH configuration file so that you can connect to a server without typing a password
 
 file_line {'ssh_config':
-    path  => '~/etc/ssh/ssh_config',
-    match => '*Pass*Auth*yes',
-    line  => '#   PasswordAuthentication no'
+    path   => '/etc/ssh/ssh_config',
+    line   => '#   PasswordAuthentication yes',
+	ensure => 'absent',
+	enable => 'present',
 }
 
-file_line {'ssh_config':
-    path  => '~/etc/ssh/ssh_config',
-    match => '*Id*F*_rsa',
-    line  => '#   IdentityFile ~/.ssh/holberton'
+file_line {'ssh_config_1':
+    path    => '/etc/ssh/ssh_config',
+    match   => '#   IdentityFile ~/.ssh/id_rsa',
+	line    => '#   IdentityFile ~/.ssh/holberton',
+	enable => 'present',
 }
