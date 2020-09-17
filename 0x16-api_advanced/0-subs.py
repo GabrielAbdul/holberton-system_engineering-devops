ba857a12d6f5f298db6bcc87a93f9a91b6a64bc2
GabrielAbdul <gabrielabdul1@gmail.com>
2020-09-17 22:24:08 +0000

    shortened

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,10 @@ def number_of_subscribers(subreddit):
     '''Gets number of subscribers from subreddit'''
     import requests
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': ''}
-    subreddit_info = requests.get\
-    (url, allow_redirects=False, headers=headers).json()
+    h = {'User-Agent': ''}
+    subreddit_info = requests.get(url, allow_redirects=False, headers=h).json()
     try:
-        subscriber_count = subreddit_info.get('data').get('subscribers')
+        subscriber_count = subreddit_info['data']['subscribers']
     except AttributeError:
         return 0
     return subscriber_count
