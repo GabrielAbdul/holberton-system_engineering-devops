b48b476bf9badbda8a9cb1f2e20651431dba6aec
gabrielabdul <gabrielabdul1@gmail.com>
2020-09-23 15:26:36 +0000

    initial commit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,19 +2,30 @@
 '''Function that queries the Reddit API'''
 
 
-def recurse(subreddit, hot_list=[], a=''):
-    '''Gets number of subscribers from subreddit'''
+def count_words(subreddit, word_list):
     import requests
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     h = {'User-Agent': ''}
-    p = {'after': a}
+    p = {'limit': 100}
+    count = 0
+    for word in word_list:
+        word = word.lower()
     info = requests.get(url, allow_redirects=False, headers=h, params=p).json()
+    word_count_dict = {}
     if info.get('message') == 'Not found':
         return None
     if info.get('data') and info['data'].get('children') is not None:
         children = info.get('data').get('children')
-        hot_list += [title['data']['title'] for title in children]
+        for dictionary in children:
+            title = dictionary.get('data').get('title').lower()
+            title = title.split()
+            for word in word_list:
+                if word in title:
+                    count += 1
+                    word_count_dict.update({word: count})
         a = info['data']['after']
-        return recurse(subreddit, hot_list, a) if a is not None else hot_list
-    else:
-        return None
+        if a is not None:
+            return count_words(subreddit, word_list)
+        else:
+            for word in word_count_dict:
+                print('{}: {}'.format(word, word_count_dict[word]))
