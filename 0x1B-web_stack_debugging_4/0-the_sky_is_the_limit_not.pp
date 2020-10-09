9b41625c0b90784829304bb734f773d2fd5decb8
gabrielabdul <gabrielabdul1@gmail.com>
2020-10-09 18:26:07 +0000

    added /g to specify global replace

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Sky is the limit, let's bring that limit higher
 exec { 'replace files per process':
-  command => "sudo sed -i '/^ULIMIT/s/^/#/' /etc/default/nginx",
+  command => "sudo sed -i '/^ULIMIT/s/^/#/g' /etc/default/nginx",
   path    =>   ['/bin', '/usr/bin'],
 }
 exec {'restart nginx service':
