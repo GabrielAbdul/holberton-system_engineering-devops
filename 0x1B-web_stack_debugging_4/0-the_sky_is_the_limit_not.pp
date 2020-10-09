bc3c0200e2a85f6159e2e070f9cfdfae20edda77
gabrielabdul <gabrielabdul1@gmail.com>
2020-10-09 18:50:53 +0000

    increased number of worker processes nginx allowed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,8 @@
 # Sky is the limit, let's bring that limit higher
-
 exec { 'replace files per process':
-  command => "sudo sed -i '/^ULIMIT/s/^/# /' /etc/default/nginx",
+  command => "sudo sed -i 's/worker_processes 4/worker_processes 2048/g' /etc/nginx/nginx.conf",
   path    =>   ['/bin', '/usr/bin'],
 }
-
 exec {'restart nginx service':
   command =>  'sudo service nginx restart',
   path    => '/usr/bin',
