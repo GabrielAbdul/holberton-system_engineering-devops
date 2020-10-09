3e7aa37438f93517e588198bbe06303e81b43941
gabrielabdul <gabrielabdul1@gmail.com>
2020-10-09 18:35:21 +0000

    changes'

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,14 @@
 # Sky is the limit, let's bring that limit higher
+
 exec { 'replace files per process':
   command => "sudo sed -i '/^ULIMIT/s/^/#/g' /etc/default/nginx",
   path    =>   ['/bin', '/usr/bin'],
 }
+
 exec {'restart nginx service':
   command =>  'sudo service nginx restart',
-  path    => '/usr/bin',
+  path    => ['/sbin', '/usr/bin', '/usr/sbin'],
+} ~>
+service {'nginx':
+  ensure => running,
 }
