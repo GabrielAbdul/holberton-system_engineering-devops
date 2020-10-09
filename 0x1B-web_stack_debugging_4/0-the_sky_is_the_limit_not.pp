cc6beb0af6252d8251839a53b6bb9bcf176c2c7b
gabrielabdul <gabrielabdul1@gmail.com>
2020-10-09 18:22:55 +0000

    went from using sed to commenting out ULIMIT line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Sky is the limit, let's bring that limit higher
 exec { 'replace files per process':
-  command => "sudo sed -i 's/15/4096/g' /etc/default/nginx",
+  command => "sudo sed -i /^ULIMIT/s/^/#/ /etc/default/nginx",
   path    =>   ['/bin', '/usr/bin'],
 }
 exec {'restart nginx service':
