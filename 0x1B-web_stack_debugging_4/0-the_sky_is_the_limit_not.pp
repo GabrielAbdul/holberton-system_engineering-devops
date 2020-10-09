ccca87dd7f1f6daa27cad599936cb82568ab114b
gabrielabdul <gabrielabdul1@gmail.com>
2020-10-09 18:41:48 +0000

    added space after '#' comment line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,11 @@
 # Sky is the limit, let's bring that limit higher
 
 exec { 'replace files per process':
-  command => "sudo sed -i '/^ULIMIT/s/^/#/g' /etc/default/nginx",
+  command => "sudo sed -i '/^ULIMIT/s/^/# /' /etc/default/nginx",
   path    =>   ['/bin', '/usr/bin'],
 }
 
 exec {'restart nginx service':
   command =>  'sudo service nginx restart',
-  path    => ['/sbin', '/usr/bin', '/usr/sbin'],
-} ~>
-service {'nginx':
-  ensure => running,
+  path    => '/usr/bin',
 }
